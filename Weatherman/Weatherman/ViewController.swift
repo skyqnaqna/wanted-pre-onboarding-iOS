c5f34ef0621273be848f23cdac2b3d80cda97f23
Subin <skyqnaqna@naver.com>
2022-09-10 13:22:25 +0900

    프로젝트 생성

---
+++ Weatherman/Weatherman/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Weatherman
+//
+//  Created by Subin Kim on 2022/09/10.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
